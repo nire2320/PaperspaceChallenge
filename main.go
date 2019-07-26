e60a3c6077d2de01348f9601eee453a693533ba8
gggorillaz <gggorillaz@gmail.com>
2019-07-26 16:20:42 -0400

    Adding in FileReader functionality. Modifying bytestrings to examine import list

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 	"io/ioutil"
 	"log"
 	"strings"
+	//"encoding/json"
 )
+
+//Our JSON data structure
+/*type JSONFiles struct {
+	filename string
+	packages []string
+}
+*/
 
 func main() {
 
@@ -16,14 +24,30 @@ func main() {
 
 	for _, f := range files {
 		if strings.Contains(f.Name(), ".go") {
-			content, err := ioutil.ReadFile(f.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
+			//content, err := ioutil.ReadFile(f.Name())
+			content := importFileReader(f.Name())
+
 			fmt.Println("File " + f.Name() + " Go File Opened!")
 			fmt.Println(f.Name() + " Contents: " + string(content))
 		}
 	}
+}
+
+func importFileReader(f string) []byte {
+
+	byteData, err := ioutil.ReadFile(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dataToString := string(byteData)
+
+	if strings.Contains(dataToString, "import") {
+		//Modify the data string by chopping off the prefix of anything before the string "import" (this will be buggy later if files contain comments with "import" in them...)
+		dataToString = strings.TrimPrefix(dataToString, dataToString[:strings.Index(dataToString, "import")])
+
+		return []byte(dataToString)
+	}
+	return []byte("0")
 }
 
 //for each file in dir ---> DONE
@@ -33,10 +57,6 @@ func main() {
 /* EX :
 {
 	"Filename":"Filename1",
-	"Filename1Packs": ["Package1":"fmt", "Package2":"math", "Package3":"string"]
-	  "File2": {
-		"Package1":"fmt",
-		"Package2":"string"
-	  }
+	"Packages": ["fmt", "math", "string"]
 }
 */
