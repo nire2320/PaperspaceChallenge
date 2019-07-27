b2aacc7c084d91a9f9e4b7db643c7537f93bbe69
gggorillaz <gggorillaz@gmail.com>
2019-07-27 11:51:35 -0400

    New test file. Working with ONLY bytedata now (some bugs otherwise). Only JSON structure output remains!

---
+++ doubleImport.go
@@ -0,0 +1,14 @@
+/*
+	Testing multiple packages
+*/
+
+package main
+
+import "fmt"
+import "math"
+
+func doubleImport() {
+
+	fmt.Println("Hello World!")
+	fmt.Println(math.Abs(-1))
+}
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"strings"
 	//"encoding/json"
 )
 
 //Our JSON data structure
-/*type JSONFiles struct {
+/*type JSONFile struct {
 	filename string
 	packages []string
-}
-*/
+}*/
 
 func main() {
 
@@ -26,14 +27,15 @@ func main() {
 		if strings.Contains(f.Name(), ".go") {
 			//content, err := ioutil.ReadFile(f.Name())
 			content := importFileReader(f.Name())
-			fmt.Println("File " + f.Name() + " Go File Opened!")
-			for i := range content {
-				if content[i] == ")" {
-					content = content[:i]
-					break
+
+			for i := len(content) - 1; i >= 0; i-- {
+				if content[i] == "import" || content[i] == "(" || content[i] == ")" {
+					content = append(content[:i], content[i+1:]...)
 				}
 			}
-			fmt.Println(content[1:])
+			fmt.Println("File " + f.Name() + " Go File Opened!")
+
+			fmt.Println(content)
 			fmt.Println()
 		}
 	}
@@ -41,19 +43,24 @@ func main() {
 
 func importFileReader(f string) []string {
 
-	byteData, err := ioutil.ReadFile(f)
+	doubleNewline := []byte{13, 10, 13, 10}              //"\n\n" byte characters
+	importString := []byte{105, 109, 112, 111, 114, 116} //"import" byte characters
+
+	file, err := os.Open(f)
+	byteData, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Fatal(err)
 	}
-	dataToString := string(byteData)
 
-	if strings.Contains(dataToString, "import") {
-		//Modify the data string by chopping off the prefix of anything before the string "import" (this will be buggy later if files contain comments with "import" in them...)
-		dataToString = strings.TrimPrefix(dataToString, dataToString[:strings.Index(dataToString, "import")])
-		strArr := strings.Fields(dataToString)
-		if strArr[1] == "(" {
-			return strArr[1:]
-		}
+	if strings.Contains(string(byteData), "import") {
+		//Modify the byte data by chopping off the prefix of anything before the string "import" (this will be buggy if files contain comments with "import" in them...)
+		byteData = bytes.TrimPrefix(byteData, byteData[:bytes.Index(byteData, importString)])
+
+		//Modify the byte data by chopping off the suffix of anything after a double newline character (13 10 == "\n")
+		byteData = bytes.TrimSuffix(byteData, byteData[bytes.Index(byteData, doubleNewline):])
+
+		strArr := strings.Fields(string(byteData))
+
 		return strArr
 	}
 	return nil
@@ -61,7 +68,7 @@ func importFileReader(f string) []string {
 
 //for each file in dir ---> DONE
 //read file ---> DONE
-//list all packages
+//list all packages and filename ---> DONE
 //output JSON object with Filename and all imported packages
 /* EX :
 {
